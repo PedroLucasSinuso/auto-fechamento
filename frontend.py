d49c8dc5195fbd4c8ac7576d9efd94f84c8db301
Pedro Lucas <pedrolucassinuso@gmail.com>
2025-01-21 13:45:55 -0300

    Change page config

--- frontend.py
+++ frontend.py
@@ -4,9 +4,13 @@ from dataProcess import genReport
 from dataWrite import sheetEdit
 from io import BytesIO
  
+
 # Título do aplicativo
 st.title("Auto-fechamento")
-
+st.set_page_config(
+    page_title="Auto-fechamento: Taco",
+    page_icon="🌟",
+)
 # Uploads de arquivos
 htmlFile = st.file_uploader("Faça o upload do arquivo HTML", type=["html"])
 xlsmFile = st.file_uploader("Faça o upload do arquivo XLSM", type=["xlsm"])
